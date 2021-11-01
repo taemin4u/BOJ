49bf53a3ae94b8e00f48204760a33dc2a19063ad
taemin <taemin4u@khu.ac.kr>
2021-11-02 02:49:11 +0900

    BOJ

--- Graph/BOJ 2206 벽부수고 이동/소스1.cpp
+++ Graph/BOJ 2206 벽부수고 이동/소스1.cpp
@@ -9,6 +9,7 @@ int N;
 int M;
 int map[1000][1000];
 bool visited[1000][1000][2]; // false면 방문 x
+							 // [i][j][0] 아직까지 한 번도 벽을 뚫지 않음 [i][j][1] 벽을 뚫음
 vector<pair<int, int>> oneidx;
 int dirt[4][2]{ {-1, 0}, {0, 1}, {1, 0}, {0, -1} }; // 북동남서
 
@@ -35,12 +36,12 @@ int BFS() { // (N, M)의 cost를 반환
 		for (int i = 0; i < 4; ++i) {
 			int dy = tinfo.y + dirt[i][0];
 			int dx = tinfo.x + dirt[i][1];
-			if (dx >= 0 && dy >= 0 && dx < M && dy < N && visited[dy][dx][bw] == false) {
-				if (map[dy][dx] == 0) {
+			if (dx >= 0 && dy >= 0 && dx < M && dy < N && visited[dy][dx][bw] == false) { // 방문하지 않은 경우
+				if (map[dy][dx] == 0) { // 0이어서 방문 가능한 경우
 					visited[dy][dx][bw] = true;
 					q.push(Info(dy, dx, tinfo.cost + 1, bw));
 				}
-				else if (bw == false) {
+				else if (bw == false) { // 아직까지 뚫은 벽이 없어 벽을 뚫음으로서 방문 가능한 경우
 					visited[dy][dx][true] = true;
 					q.push(Info(dy, dx, tinfo.cost + 1, true));
 				}
@@ -65,9 +66,9 @@ int main() {
 			}
 		}
 	}
-	
+
 	cout << BFS();
-	
-	
+
+
 	return 0;
 }
